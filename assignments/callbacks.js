1ddabbf0865a94abc2f44ddd0a0a91f3fb43cfb0
Kenna <kennaalastair@gmail.com>
2019-06-11 16:02:36 -0700

    finished challenges for daily project javascript II

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,54 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length){
+  console.log(length);
+})
+
+//--------------------------------------------------------------------------------------
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  first = arr.length - 1;
+  return cb(arr[first]);
 }
+last(items, lastItem => {
+  console.log(lastItem);
+})
+
+//--------------------------------------------------------------------------------------
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  sum = x + y;
+  return cb(sum);
 }
+
+sumNums(2,3, eqsum => {
+  console.log(eqsum);
+})
+
+//--------------------------------------------------------------------------------------
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  product = x * y;
+  return cb(product);
 }
+
+multiplyNums(2,6, eqprod => {
+  console.log(eqprod);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list);
 }
+
+console.log(items.includes('yo-yo'));
 
 /* STRETCH PROBLEM */
 
